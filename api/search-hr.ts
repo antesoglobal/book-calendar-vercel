f3ce76a04d900db8dd6ad707d5bf95261b8d0ac2
An Nguyen <an@tesosoft.com>
2025-10-26 13:31:05 +0700

    Change deployment google script link

--- api/search-hr.ts
+++ api/search-hr.ts
@@ -10,7 +10,9 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   
   console.log("Search-HR called with:", name, department); // 👈 log để hiện trong Vercel
   
-  const url = new URL("https://script.google.com/macros/s/AKfycbyoCnffZV75cqBVjhDtkKD-zuAGx9_7v0geCLYaVpPn0WtwclY0g-I49IATH5GFh838/exec");
+  // const url = new URL("https://script.google.com/macros/s/AKfycbyoCnffZV75cqBVjhDtkKD-zuAGx9_7v0geCLYaVpPn0WtwclY0g-I49IATH5GFh838/exec");
+  const url = new URL("https://script.google.com/macros/s/AKfycbyjEv7egzYsIwsBmzNd0VPKPH33mCXRhTYUMJIRUHbap7bHA4h_55NfgKb0IqvWm9sH/exec");
+  
 
   if (name) url.searchParams.append("name", name);
   if (department) url.searchParams.append("department", department);
